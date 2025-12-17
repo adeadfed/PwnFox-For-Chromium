2ba3d178a8ba68ab4be84966b1c3274ad590adfc
Maksym Vatsyk <maksym.vatsyk17@gmail.com>
2025-12-17 16:57:29 +0100

    add ability to reset profile names and locate burpbrowser automatically

--- src/main/java/com/adeadfed/PwnFoxForChromiumUI.java
+++ src/main/java/com/adeadfed/PwnFoxForChromiumUI.java
@@ -39,6 +39,8 @@ public class PwnFoxForChromiumUI {
     private JButton pinkButton;
     private JButton magentaButton;
     private JLabel helpLabel;
+    private JButton resetProfileNamesButton;
+    private JButton locateBurpBrowserButton;
 
     public JPanel getUI() {
         return ui;
@@ -59,6 +61,47 @@ public class PwnFoxForChromiumUI {
             uiPath.setText(path);
         }
         button.addActionListener(e -> uiChoosePath(preference, uiPath, pathMode));
+    }
+
+    private void setupResetProfileNamesButton() {
+        Runnable resetProfileNamesCallback = () -> {
+            try {
+                JButton[] profileButtons = {
+                        blueButton, cyanButton, greenButton, yellowButton,
+                        redButton, orangeButton, pinkButton, magentaButton
+                };
+
+                for (JButton b: profileButtons) {
+                    String defaultColorValue = b.getName();
+                    b.setText(defaultColorValue);
+                    pwnChromiumExtension.pwnChromiumPreferences.setProfileName(b.getName(), defaultColorValue); 
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "An error resetting profile names occured. Check the extension logs");
+                pwnChromiumExtension.montoyaApi.logging().logToError(e);
+            }
+        };
+        resetProfileNamesButton.addActionListener(e -> resetProfileNamesCallback.run());
+    }
+
+    private void setupLocateBurpBrowserButton() {
+        Runnable locateBurpBrowserCallback = () -> {
+            try {
+                String burpBrowserPath = pwnChromiumExtension.pwnChromiumPreferences.browserPath.getDefault();
+                if (burpBrowserPath == null) {
+                    throw new Exception("Failed to locate BurpBrowser automatically.");
+                }
+                pwnChromiumExtension.pwnChromiumPreferences.set(
+                    pwnChromiumExtension.pwnChromiumPreferences.browserPath,
+                    burpBrowserPath
+                );
+                pwnChromeExePath.setText(burpBrowserPath);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "An error locating BurpBrowser occured. Check the extension logs");
+                pwnChromiumExtension.montoyaApi.logging().logToError(e);
+            }
+        };
+        locateBurpBrowserButton.addActionListener(e -> locateBurpBrowserCallback.run());
     }
 
     private void setupProfileButtons() {
@@ -185,6 +228,9 @@ public class PwnFoxForChromiumUI {
 
         setupProfileButtons();
         setupRenameHelpLabel();
+
+        setupResetProfileNamesButton();
+        setupLocateBurpBrowserButton();
     }
 
     {
@@ -204,10 +250,10 @@ public class PwnFoxForChromiumUI {
     private void $$$setupUI$$$() {
         Color white = new Color(-1);
         ui = new JPanel();
-        ui.setLayout(new GridLayoutManager(4, 2, new Insets(30, 30, 30, 30), -1, -1));
+        ui.setLayout(new GridLayoutManager(5, 4, new Insets(30, 30, 30, 30), -1, -1));
         settingsPanel = new JPanel();
-        settingsPanel.setLayout(new GridLayoutManager(6, 3, new Insets(20, 0, 0, 0), -1, -1));
-        ui.add(settingsPanel, new GridConstraints(3, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        settingsPanel.setLayout(new GridLayoutManager(6, 4, new Insets(20, 0, 0, 0), -1, -1));
+        ui.add(settingsPanel, new GridConstraints(4, 0, 1, 4, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         final JLabel label1 = new JLabel();
         label1.setText("Path to the Chromium executable");
         settingsPanel.add(label1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
@@ -221,7 +267,7 @@ public class PwnFoxForChromiumUI {
         final Spacer spacer1 = new Spacer();
         settingsPanel.add(spacer1, new GridConstraints(5, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final Spacer spacer2 = new Spacer();
-        settingsPanel.add(spacer2, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        settingsPanel.add(spacer2, new GridConstraints(2, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JLabel label2 = new JLabel();
         Font label2Font = this.$$$getFont$$$(null, Font.BOLD, 22, label2.getFont());
         if (label2Font != null) label2.setFont(label2Font);
@@ -237,9 +283,12 @@ public class PwnFoxForChromiumUI {
         chooseProfilesDirButton = new JButton();
         chooseProfilesDirButton.setText("Choose...");
         settingsPanel.add(chooseProfilesDirButton, new GridConstraints(4, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        locateBurpBrowserButton = new JButton();
+        locateBurpBrowserButton.setText("Locate Burp Browser");
+        settingsPanel.add(locateBurpBrowserButton, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         buttonsPanel = new JPanel();
         buttonsPanel.setLayout(new GridLayoutManager(2, 4, new Insets(0, 0, 0, 0), -1, -1));
-        ui.add(buttonsPanel, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        ui.add(buttonsPanel, new GridConstraints(1, 0, 1, 4, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         blueButton = new JButton();
         blueButton.setBackground(ProfileColors.BLUE.getColor());
         blueButton.setBorderPainted(true);
@@ -312,12 +361,19 @@ public class PwnFoxForChromiumUI {
         Font label4Font = this.$$$getFont$$$(null, Font.BOLD, 22, label4.getFont());
         if (label4Font != null) label4.setFont(label4Font);
         label4.setText("PwnChromium Profiles");
-        ui.add(label4, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        ui.add(label4, new GridConstraints(0, 0, 1, 4, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer3 = new Spacer();
-        ui.add(spacer3, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        ui.add(spacer3, new GridConstraints(2, 3, 2, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         helpLabel = new JLabel();
         helpLabel.setText("Ctrl+Click to edit button names");
-        ui.add(helpLabel, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        ui.add(helpLabel, new GridConstraints(2, 0, 2, 1, GridConstraints.ANCHOR_NORTHWEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        resetProfileNamesButton = new JButton();
+        resetProfileNamesButton.setText("Reset Profile Names");
+        ui.add(resetProfileNamesButton, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer4 = new Spacer();
+        ui.add(spacer4, new GridConstraints(3, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final Spacer spacer5 = new Spacer();
+        ui.add(spacer5, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         label1.setLabelFor(pwnChromeExePath);
         label3.setLabelFor(pwnChromeProfilesPath);
     }
